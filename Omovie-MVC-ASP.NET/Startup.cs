4c0d8e18238ff173f80ef3e4db98b07ff5750be5
JoeyDon <joey.don0905@gmail.com>
2018-07-23 12:00:22 +0800

    Add project files.

---
+++ Omovie-MVC-ASP.NET/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Omovie_MVC_ASP.NET.Startup))]
+namespace Omovie_MVC_ASP.NET
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
